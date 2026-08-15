0fb79a459ac2732caff56240f59f97d9c427bb82
mayzula7-cloud <mayzula7@gmail.com>
2026-08-15 06:50:53 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,7 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "sb_publishable_4n64k5NM0t12Nat7aqqkzw_4FraK6IH
-";
+const SUPABASE_URL = "sb_publishable_4n64k5NM0t12Nat7aqqkzw_4FraK6IH";
 const SUPABASE_KEY = "COLLE_TA_CLE_PUBLIABLE_ICI";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
